ea1b6835a36a6ea83f8606ae05a2ea2d1fba794f
Carlos Gerardo <carlosgerardo.rocha.s@gmail.com>
2022-04-11 21:21:00 -0500

    first commit

---
+++ 01-hola-mundo/app.js
@@ -0,0 +1,8 @@
+let  nombre = 'Carlos';
+console.log(nombre)
+
+nombre = 'Garardo'
+
+console.log(nombre)
+
+console.log('hola mundo')
---
+++ 01-hola-mundo/app3.js
@@ -0,0 +1,18 @@
+console.log(`Inicio del programa`)
+
+setTimeout(()=>{
+console.log(`Primer TimeOut`)
+},3000) ; 
++
+setTimeout(()=>{
+console.log(`segundo TimeOut`)
+},0) ; 
+
+
+setTimeout(()=>{
+console.log(`tercer TimeOut`)
+},0) ; 
+
+console.log(`fin  de programa`)
+
+
